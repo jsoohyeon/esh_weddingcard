026ac0ad0792e0b20a43d640be7a47d40e829ecf
주수현 <jusuhyeon@jusuhyeon-ui-MacBookPro.local>
2021-07-01 22:03:10 +0900

    사진변경

--- src/Main.js
+++ src/Main.js
@@ -9,44 +9,30 @@ function main(){
         <div>
             <div>
                 <div>
-                <img src = {mainimg} className = "Main-photo" alt = "main"/>
+                    <img src = {mainimg} className = "Main-photo" alt = "main"/>
                     <div className="Main-photo-text">
-                    <span /*className = "Main-photo-text"*/>⎽⎽</span><br/>
-                        09 <br/>
-                        月 <br/>
-                        04 <br/>
-                        日 <br/>
-                    <span /*className = "Main-photo-text"*/>⎺⎺</span>
+                        <span /*className = "Main-photo-text"*/>⎽⎽</span><br/>
+                            09 <br/>
+                            月 <br/>
+                            04 <br/>
+                            日 <br/>
+                        <span /*className = "Main-photo-text"*/>⎺⎺</span>
                     </div>
                 </div>
 
-<<<<<<< HEAD
                 <div className = "Main-text-group">
                     <p className = "Main-text-name">
                         김응수&nbsp;&nbsp;&nbsp;ෆ&nbsp;&nbsp;&nbsp;주수현
                     </p>
-=======
-            <div className = "Main-text-group">
-                <p className = "Main-text-name">
-                    김응수&nbsp;&nbsp;&nbsp;ෆ&nbsp;&nbsp;&nbsp;주수현
-                </p>
->>>>>>> a2f00c852419acc60e44a99903c9fcb4841ebf3f
 
                     <p className = "Main-text">
                         2021. 09. 04 SAT AM 11:30
                     </p>
 
-<<<<<<< HEAD
                     <p className = "Main-text">
                         그랜드힐 컨벤션 2층 사브리나홀
                     </p>
                 </div>
-=======
-                <p className = "Main-text">
-                    그랜드힐 컨벤션 2층 사브리나홀
-                </p>
-            </div>
->>>>>>> a2f00c852419acc60e44a99903c9fcb4841ebf3f
 
                 <row>
                     <img src = {eungself} className = "Self-photo-right" alt = "Eungsoo"/>
@@ -71,16 +57,11 @@ function main(){
                     김효식 ⋄ 신미경 <span>의 아들</span> 김응수 <br/>
                     주용회 ⋄ 고점숙 <span>의 딸</span> 주수현
                 </div>
-<<<<<<< HEAD
-                <a href="#popup--1" className = "btn--contact">신랑 신부에게 연락하기</a>
-                <div className = "popup--1"> 신랑신부에게 연락처 </div>
-=======
 
                 <a href="#popup--1" className = "btn--contact">신랑 신부에게 연락하기</a>
                 <div className = "popup--1"> 신랑신부에게 연락처 </div>
                
 
->>>>>>> a2f00c852419acc60e44a99903c9fcb4841ebf3f
             </section>
         </div>
     )
